33e883a83beacac99e5a45d08e193f31ee74924b
方浩 <hfang65@best-inc.com>
2024-11-25 17:07:38 +0800

    feat: ts 声明

---
+++ env.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_SUPABASE_URL: string;
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+    NEXT_PUBLIC_TAG: string;
+    // ... 其他环境变量
+  }
+}
--- next-env.d.ts
+++ next-env.d.ts
@@ -5,12 +5,3 @@
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
 // see https://nextjs.org/docs/pages/building-your-application/configuring/typescript for more information.
-
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NEXT_PUBLIC_SUPABASE_URL: string;
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
-    NEXT_PUBLIC_TAG: string;
-    // ... 其他环境变量
-  }
-}
